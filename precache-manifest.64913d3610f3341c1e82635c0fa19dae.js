a99609725c337545540e38500d645bc3ef0c5979
tieunt97 <tieunt.bk97@gmail.com>
2018-11-16 13:55:21 +0700

    Updates

---
+++ precache-manifest.64913d3610f3341c1e82635c0fa19dae.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fb84a915f61f0381f1e57aacf58034a",
+    "url": "/portfolio/static/media/Quote_8_Eleanor_Roosevelt.2fb84a91.jpg"
+  },
+  {
+    "revision": "e478c30e898e9f7a0fba",
+    "url": "/portfolio/static/css/main.a70e2e14.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "67a701a4d251dca4024a",
+    "url": "/portfolio/static/js/1.67a701a4.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "adef3aa36947b36d4836b7eaef76a3a5",
+    "url": "/portfolio/static/media/tieunt.adef3aa3.jpg"
+  },
+  {
+    "revision": "a35afd3c15d4d90557d6aeaaad1af5ac",
+    "url": "/portfolio/static/media/Student_Management_System.a35afd3c.png"
+  },
+  {
+    "revision": "e478c30e898e9f7a0fba",
+    "url": "/portfolio/static/js/main.e478c30e.chunk.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "67a701a4d251dca4024a",
+    "url": "/portfolio/static/css/1.af035106.chunk.css"
+  },
+  {
+    "revision": "a173058c833cdd7c7125d37311df11d7",
+    "url": "/portfolio/index.html"
+  }
+];
